e66eb7d7a0c15ed2960989e3428bda36205a8ed1
Cleyton Silva <cleyton.eu123@gmail.com>
2023-05-25 12:27:13 -0300

    adding AR.js integration

--- src/client/client.ts
+++ src/client/client.ts
@@ -7,41 +7,138 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import * as CANNON from 'cannon-es'
 import CannonUtils from './utils/cannonUtils'
 import CannonDebugRenderer from './utils/cannonDebugRenderer'
+import { THREEx, ARjs } from "@ar-js-org/ar.js-threejs"
+
+THREEx.ArToolkitContext.baseURL = "./";
+
+// array of functions for the rendering loop
+var onRenderFcts: any[] = [];
+var arToolkitContext: any, arMarkerControls;
+
+var arToolkitSource = new THREEx.ArToolkitSource({
+  sourceType: 'webcam',
+
+  sourceWidth: window.innerWidth > window.innerHeight ? 640 : 480,
+  sourceHeight: window.innerWidth > window.innerHeight ? 480 : 640,
+})
+
+arToolkitSource.init(function onReady() {
+    
+  arToolkitSource.domElement.addEventListener('canplay', () => {
+      console.log(
+        'canplay',
+        'actual source dimensions',
+        arToolkitSource.domElement.videoWidth,
+        arToolkitSource.domElement.videoHeight,
+      );
+
+      initARContext();
+  }) as unknown as HTMLVideoElement;
+  window.arToolkitSource = arToolkitSource;
+  setTimeout(() => {
+    onWindowResize()
+  }, 2000);
+}, function onError() { })
+
+function initARContext() { // create atToolkitContext
+  arToolkitContext = new THREEx.ArToolkitContext({
+      cameraParametersUrl: THREEx.ArToolkitContext.baseURL + 'data/camera_para.dat',
+      detectionMode: 'mono',
+  })
+
+  // initialize it
+  arToolkitContext.init(() => { // copy projection matrix to camera
+      camera.projectionMatrix.copy(arToolkitContext.getProjectionMatrix());
+
+      arToolkitContext.arController.orientatio = getSourceOrientation();
+      arToolkitContext.arController.options.orientation = getSourceOrientation();
+
+      console.log('arToolkitContext', arToolkitContext);
+      window.arToolkitContext = arToolkitContext;
+  })
+
+  // MARKER
+  arMarkerControls = new THREEx.ArMarkerControls(arToolkitContext, camera, {
+      type: 'pattern',
+      patternUrl: THREEx.ArToolkitContext.baseURL + './data/patt.hiro',
+      // patternUrl : THREEx.ArToolkitContext.baseURL + '../data/data/patt.kanji',
+      // as we controls the camera, set changeMatrixMode: 'cameraTransformMatrix'
+      changeMatrixMode: 'cameraTransformMatrix',
+  })
+
+  scene.visible = false
+
+  console.log('ArMarkerControls', arMarkerControls);
+  window.arMarkerControls = arMarkerControls;
+}
+
+function getSourceOrientation(): string {
+  if (!arToolkitSource) {
+      return '';
+  }
+
+  console.log(
+      'actual source dimensions',
+      arToolkitSource.domElement.videoWidth,
+      arToolkitSource.domElement.videoHeight
+  );
+
+  if (arToolkitSource.domElement.videoWidth > arToolkitSource.domElement.videoHeight) {
+      console.log('source orientation', 'landscape');
+      return 'landscape';
+  } else {
+      console.log('source orientation', 'portrait');
+      return 'portrait';
+  }
+}
+
+window.addEventListener("markerFound", function (e) {
+  planeMesh.visible = false;
+  console.log("marker found!", e);
+})
+
+onRenderFcts.push(function () {
+  if (!arToolkitContext || !arToolkitSource || !arToolkitSource.ready) {
+      return;
+  }
+
+  arToolkitContext.update(arToolkitSource.domElement)
+
+  // update scene.visible if the marker is seen
+  scene.visible = camera.visible
+})
+
 
 const scene = new THREE.Scene()
 scene.add(new THREE.AxesHelper(5))
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+)
+camera.position.set(0, 10, 9);
+var target = new THREE.Vector3(0, 10, 0);
+camera.lookAt(target);
 
-const light1 = new THREE.SpotLight()
+const light1 = new THREE.AmbientLight()
 light1.position.set(2.5, 5, 5)
-light1.angle = Math.PI / 4
-light1.penumbra = 0.5
-light1.castShadow = true
-light1.shadow.mapSize.width = 1024
-light1.shadow.mapSize.height = 1024
-light1.shadow.camera.near = 0.5
-light1.shadow.camera.far = 20
+light1.intensity = 0.6
 scene.add(light1)
 
 const light2 = new THREE.SpotLight()
-light2.position.set(-2.5, 5, 5)
-light2.angle = Math.PI / 4
+light2.position.set(-2.5, 10, 5)
+light2.angle = Math.PI/4
 light2.penumbra = 0.5
 light2.castShadow = true
+light2.intensity = 0.3
 light2.shadow.mapSize.width = 1024
 light2.shadow.mapSize.height = 1024
 light2.shadow.camera.near = 0.5
 light2.shadow.camera.far = 20
 scene.add(light2)
 
-const camera = new THREE.PerspectiveCamera(
-    75,
-    window.innerWidth / window.innerHeight,
-    0.1,
-    1000
-)
-camera.position.set(0, 10, 9);
-var target = new THREE.Vector3(0, 10, 0);
-camera.lookAt(target);
+
 declare global {
     interface Window {
         testVar: any;
@@ -53,6 +150,13 @@ window.testVar = camera;
 const renderer = new THREE.WebGLRenderer()
 renderer.setSize(window.innerWidth, window.innerHeight)
 renderer.shadowMap.enabled = true
+renderer.setClearColor(new THREE.Color('lightgrey'), 0)
+renderer.setSize(640, 480);
+renderer.domElement.style.position = 'absolute'
+renderer.domElement.style.top = '0px'
+renderer.domElement.style.left = '0px'
+document.body.appendChild(renderer.domElement);
+
 document.body.appendChild(renderer.domElement)
 
 const controls = new OrbitControls(camera, renderer.domElement)
@@ -60,23 +164,77 @@ controls.enableDamping = true
 controls.target.y = 0.5
 
 const world = new CANNON.World()
+const wireframeEnabled = false;
+world.broadphase = new CANNON.SAPBroadphase(world)
 world.gravity.set(0, -9.82, 0)
 // world.broadphase = new CANNON.NaiveBroadphase()
 // ;(world.solver as CANNON.GSSolver).iterations = 10
 // world.allowSleep = true
 
-const normalMaterial = new THREE.MeshNormalMaterial()
-const phongMaterial = new THREE.MeshPhongMaterial()
+const defaultMaterial = new CANNON.Material('default')
+const defaultContactMaterial = new CANNON.ContactMaterial(
+    defaultMaterial,
+    defaultMaterial,
+    {
+        friction: 0.1,
+        restitution: 0.1
+    }
+)
+world.defaultContactMaterial = defaultContactMaterial
+
+const textureLoader = new THREE.TextureLoader()
+const tenisTexture = textureLoader.load('/textures/objects/tenisball.jpg');
+const woodTexture = textureLoader.load('/textures/objects/wood.jpg');
+const floorTexture = textureLoader.load('/textures/objects/floor.jpg');
+const marmorTexture = textureLoader.load('/textures/objects/marmor.jpg');
+const cubeTextureLoader = new THREE.CubeTextureLoader()
+
+const environmentMapTexture = cubeTextureLoader.load([
+  '/textures/environmentMaps/0/px.png',
+  '/textures/environmentMaps/0/nx.png',
+  '/textures/environmentMaps/0/py.png',
+  '/textures/environmentMaps/0/ny.png',
+  '/textures/environmentMaps/0/pz.png',
+  '/textures/environmentMaps/0/nz.png'
+])
+
+const threeMaterial = new THREE.MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+  envMapIntensity: 0.5
+})
+
+const woodMaterial = new THREE.MeshStandardMaterial({
+  map: woodTexture,
+  envMap: environmentMapTexture,
+  envMapIntensity: 0.5
+})
+
+const tenisballMaterial =  new THREE.MeshStandardMaterial({
+  roughness: 1,
+  map: tenisTexture,
+  envMap: environmentMapTexture,
+  envMapIntensity: 0.5
+})
+
+const marmorMaterial =  new THREE.MeshStandardMaterial({
+  metalness: 0.7,
+  roughness: 1,
+  map: marmorTexture,
+  envMap: environmentMapTexture,
+  envMapIntensity: 0.5
+})
 
 const sphereGeometry = new THREE.SphereGeometry(0.5)
-const sphereMesh = new THREE.Mesh(sphereGeometry, normalMaterial)
+const sphereMesh = new THREE.Mesh(sphereGeometry, tenisballMaterial)
 sphereMesh.position.x = -4.99
 sphereMesh.position.y = 9
 sphereMesh.position.z = 0.5
 sphereMesh.castShadow = true
 scene.add(sphereMesh)
 const sphereShape = new CANNON.Sphere(0.5)
-const sphereBody = new CANNON.Body({ mass: 60 })
+const sphereBody = new CANNON.Body({ mass: 60, material: defaultMaterial, linearDamping: 0.1, angularDamping: 0.101 })
 sphereBody.addShape(sphereShape)
 sphereBody.position.x = sphereMesh.position.x
 sphereBody.position.y = sphereMesh.position.y
@@ -110,13 +268,13 @@ const rampExtrudeSettings = {
 };
 
 const rampGeometry = new THREE.ExtrudeGeometry( rampShape, rampExtrudeSettings );
-const rampMesh = new THREE.Mesh( rampGeometry, normalMaterial );
+const rampMesh = new THREE.Mesh( rampGeometry, woodMaterial );
 rampMesh.position.x = -2;
 
 scene.add(rampMesh);
 
 const rampCannonShape = CannonUtils.CreateTrimesh(rampGeometry)
-const rampCannonBody = new CANNON.Body({ mass: 0 })
+const rampCannonBody = new CANNON.Body({ mass: 0, material: defaultMaterial })
 rampCannonBody.addShape(rampCannonShape)
 rampCannonBody.position.x = rampMesh.position.x
 rampCannonBody.position.y = rampMesh.position.y
@@ -143,7 +301,7 @@ loader.load(
                 m.position.x = 4;
                 m.position.y = 0;
                 m.position.z = 0.5;
-                m.material = normalMaterial;
+                m.material =marmorMaterial;
                 rampContainerMesh = m;
                 rampContainerBody = new CANNON.Body({ mass: 0 });
                 const rampContainerCannonShape = CannonUtils.CreateTrimesh(m.geometry);
@@ -177,7 +335,7 @@ loader.load(
 )
 
 const planeGeometry = new THREE.PlaneGeometry(25, 25)
-const planeMesh = new THREE.Mesh(planeGeometry, phongMaterial)
+const planeMesh = new THREE.Mesh(planeGeometry, threeMaterial)
 planeMesh.position.y = -0.01
 planeMesh.rotateX(-Math.PI / 2)
 planeMesh.receiveShadow = true
@@ -190,6 +348,11 @@ world.addBody(planeBody)
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
+    arToolkitSource.onResizeElement()
+    arToolkitSource.copyElementSizeTo(renderer.domElement)
+    if (window.arToolkitContext.arController !== null) {
+        arToolkitSource.copyElementSizeTo(window.arToolkitContext.arController.canvas)
+    }
     camera.aspect = window.innerWidth / window.innerHeight
     camera.updateProjectionMatrix()
     renderer.setSize(window.innerWidth, window.innerHeight)
@@ -217,15 +380,19 @@ let delta
 
 const cannonDebugRenderer = new CannonDebugRenderer(scene, world)
 
-function animate() {
-    requestAnimationFrame(animate)
-
+var lastTimeMsec: number;
+requestAnimationFrame(animate);
+function animate(nowMsec: number) {
+    // keep looping
+    requestAnimationFrame(animate);
     controls.update()
 
     delta = Math.min(clock.getDelta(), 0.1)
     world.step(delta)
 
-    cannonDebugRenderer.update()
+    if(wireframeEnabled) {
+      cannonDebugRenderer.update()
+    }
 
         sphereMesh.position.set(
         sphereBody.position.x,
@@ -252,6 +419,14 @@ function animate() {
             rampContainerBody.quaternion.w
         )
     }
+    
+    lastTimeMsec = lastTimeMsec || nowMsec - 1000 / 60
+    var deltaMsec = Math.min(200, nowMsec - lastTimeMsec)
+    lastTimeMsec = nowMsec
+
+    onRenderFcts.forEach(function (onRenderFct) {
+      onRenderFct(deltaMsec / 1000, nowMsec / 1000)
+    })
 
     render()
 
@@ -261,5 +436,3 @@ function animate() {
 function render() {
     renderer.render(scene, camera)
 }
-
-animate()
